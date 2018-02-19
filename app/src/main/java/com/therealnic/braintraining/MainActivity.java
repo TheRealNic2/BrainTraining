6bc17a63475ddebd74b0d73f2f7ea8befd7618e5
Nicola Bortolan <nicolabortolan@gmail.com>
2018-02-19 20:09:06 +0100

    Base Menu Fatta

--- app/src/main/java/com/therealnic/braintraining/MainActivity.java
+++ app/src/main/java/com/therealnic/braintraining/MainActivity.java
@@ -1,13 +1,28 @@
 package com.therealnic.braintraining;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSettings = (Button) findViewById(R.id.btnSettings);
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,SettingsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
